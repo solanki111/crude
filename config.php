e0ef1b7c1afbc52602ab13c4ee44d5c42b17f655
Ralf Bierig <rbierig@Black-Vibe.local>
2018-02-06 19:10:58 +0000

    Changing configs (CAnywhere and Azure).

--- config.php
+++ config.php
@@ -1,75 +1,19 @@
 <?php
 
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'crude');
+define('DB_PASSWORD', 'crude123');
+define('DB_NAME', 'crude');
+ 
 /* Attempt to connect to MySQL database */
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
 }
-
-//echo "dbhost = $connectstr_dbhost" . PHP_EOL;
-//echo "dbname = $connectstr_dbname" . PHP_EOL;
-//echo "username = $connectstr_dbusername" . PHP_EOL;
-//echo "password = $connectstr_dbpassword" . PHP_EOL;
-
-$pdo = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-
-if (!$pdo) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-// creating table
-
-$sql = "CREATE TABLE IF NOT EXISTS employees (
-                id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                name VARCHAR(100) NOT NULL,
-                address VARCHAR(255) NOT NULL,
-                salary INT(10) NOT NULL);";
-
-// Create student table
-$success = $pdo->query($sql);
-if ($success) {
-	echo "Table has been created";
-}
-else {
-        echo "Error creating table";  
-}
-
-
-/*try {
-    // create the database
-    $sql = "CREATE DATABASE IF NOT EXISTS $connectstr_dbname";
-    $pdo->exec($sql);
-    
-    // switch to the database
-    $sql = "use $connectstr_dbname";
-    $pdo->exec($sql);
-    
-    // create its table
-    $sql = "CREATE TABLE IF NOT EXISTS employees (
-                id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                name VARCHAR(100) NOT NULL,
-                address VARCHAR(255) NOT NULL,
-                salary INT(10) NOT NULL);";
-    $pdo->exec($sql);
-    echo "DB created successfully";
-}
-catch(PDOException $e)
-{
-    echo $sql . "<br>" . $e->getMessage();
-}*/
 
 ?>
---
+++ configAzureIncomplete.php
@@ -0,0 +1,75 @@
+<?php
+
+/* Attempt to connect to MySQL database */
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+//echo "dbhost = $connectstr_dbhost" . PHP_EOL;
+//echo "dbname = $connectstr_dbname" . PHP_EOL;
+//echo "username = $connectstr_dbusername" . PHP_EOL;
+//echo "password = $connectstr_dbpassword" . PHP_EOL;
+
+$pdo = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$pdo) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+// creating table
+
+$sql = "CREATE TABLE IF NOT EXISTS employees (
+                id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                name VARCHAR(100) NOT NULL,
+                address VARCHAR(255) NOT NULL,
+                salary INT(10) NOT NULL);";
+
+// Create student table
+$success = $pdo->query($sql);
+if ($success) {
+	echo "Table has been created";
+}
+else {
+        echo "Error creating table";  
+}
+
+
+/*try {
+    // create the database
+    $sql = "CREATE DATABASE IF NOT EXISTS $connectstr_dbname";
+    $pdo->exec($sql);
+    
+    // switch to the database
+    $sql = "use $connectstr_dbname";
+    $pdo->exec($sql);
+    
+    // create its table
+    $sql = "CREATE TABLE IF NOT EXISTS employees (
+                id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                name VARCHAR(100) NOT NULL,
+                address VARCHAR(255) NOT NULL,
+                salary INT(10) NOT NULL);";
+    $pdo->exec($sql);
+    echo "DB created successfully";
+}
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
+}*/
+
+?>
