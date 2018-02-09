e7de83306080e087830b0b896fccd45d6355d2b6
solanki <solanki@149.157.126.255>
2018-02-09 14:39:47 +0000

    added db credentials to config file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # crude
+# Student Number: 17251693
+# Name: Abhishek Singh
+# Date: 09 Feb 2018
+This is CRUDE, my first web application for CS615.
--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'YOUR LOCATION');
-define('DB_USERNAME', 'YOUR USER NAME');
-define('DB_PASSWORD', 'YOUR PASSWORD');
-define('DB_NAME', 'YOUR DATABASE NAME');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'crude');
+define('DB_PASSWORD', 'crude');
+define('DB_NAME', 'crude');
  
 /* Attempt to connect to MySQL database */
 try{
