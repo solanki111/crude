10c00f820d0245f20bac3d49c1c41dac6128bff0
solanki <solanki@149.157.126.255>
2018-02-15 16:08:11 +0000

    trying with sql code to make azure hosting work 2

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
-
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+/*
+/* Database credentials. Assuming you are running MySQL server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'crudedbs.database.windows.net');
 define('DB_USERNAME', 'rooter');
 define('DB_PASSWORD', 'crude');
@@ -15,5 +14,22 @@ try{
 } catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
 }
+*/
 
+/************************************************************************************************/
+// PHP Data Objects(PDO) Sample Code:
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:crudedbs.database.windows.net,1433; Database = crudedb", "rooter", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "rooter@crudedbs", "pwd" => "{your_password_here}", "Database" => "crudedb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:crudedbs.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
--- configAzureIncomplete.php
+++ configAzureIncomplete.php
@@ -33,21 +33,5 @@ if (!$pdo) {
 // TODO make sure DB exists
 
 // TODO create table (only once, if it does not yet exist)
-
-/************************************************************************************************/
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:crudedbs.database.windows.net,1433; Database = crudedb", "rooter", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "rooter@crudedbs", "pwd" => "{your_password_here}", "Database" => "crudedb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:crudedbs.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
  
 ?>
